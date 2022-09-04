e6eb854e4ea4ce09c3e06c3184c6fefade5355d9
Serhii <serhii.renhach.cr@gmail.com>
2022-09-04 21:37:07 +0300

    New implementation

--- 1.py
+++ 1.py
@@ -1,11 +1,10 @@
-import re
-
 
 def is_valid(password):
-    if re.match(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d{2})(?=.*[\x00-\x7F])[A-Za-z\d\x00-\x7F]{8,}$', password):
+    import re
+    if re.match(r'^(?=.*[A-Z])(?=.*\d{2})(?=.*[\x00-\x7F])[A-Za-z\d\x00-\x7F]{8,}$', password):
         return True
     else:
         return False
 
 
-print(is_valid('Qwerty1234'))
+print(is_valid('qwertYa1'))
--- 3.py
+++ 3.py
@@ -1,6 +1,3 @@
-
-
-
 # qqq = [64, 64, 65]
 # asc_abc = sorted(qqq)
 # A = pow(asc_abc[2], 2)
@@ -36,83 +33,69 @@ class Triangle():
         self.a = a
         self.b = b
         self.c = c
+        self.set_var()
 
+    def set_var(self):
+        abc = []
+        abc.append(self.a)
+        abc.append(self.b)
+        abc.append(self.c)
+        global asc_abc
+        asc_abc = sorted(abc)
+        global a, b, c
+        a = asc_abc[2]
+        b = asc_abc[0]
+        c = asc_abc[1]
+
+    def check(self):
+        for x in asc_abc:
+            if isinstance(x, (int, float)):
+                if a + b > c and b + c > a and a + c > b and a * b * c > 0:
+                    return True
+        else:
+            print('Such a triangle cannot exist.')
 
     def perimetr(self):
-        abc = []
-        abc.append(self.a)
-        abc.append(self.b)
-        abc.append(self.c)
-        asc_abc = sorted(abc)
-        a = asc_abc[2]
-        b = asc_abc[0]
-        c = asc_abc[1]
-        if a + b > c and b + c > a and a + c > b:
+        if self.check():
             P = a + b + c
             print(f"Perimetr = {P}")
-        else:
-            print('Such a triangle cannot exist.')
-
 
     def square(self):
-        abc = []
-        abc.append(self.a)
-        abc.append(self.b)
-        abc.append(self.c)
-        asc_abc = sorted(abc)
-        a = asc_abc[2]
-        b = asc_abc[0]
-        c = asc_abc[1]
-        if a + b > c and b + c > a and a + c > b:
+        if self.check():
             P = a + b + c
-            p = P/2
-            S = (p*(p-a)*(p-b)*(p-c))*0.5
+            p = P / 2
+            S = (p * (p - a) * (p - b) * (p - c)) * 0.5
             print(f"Square = {S}")
-        else:
-            print('Such a triangle cannot exist.')
 
     def type(self):
-        abc = []
-        abc.append(self.a)
-        abc.append(self.b)
-        abc.append(self.c)
-        asc_abc = sorted(abc)
-        A = pow(asc_abc[2], 2)
-        B = pow(asc_abc[0], 2)
-        C = pow(asc_abc[1], 2)
-        a = asc_abc[2]
-        b = asc_abc[0]
-        c = asc_abc[1]
-        if a + b > c and b + c > a and a + c > b:
+        if self.check():
+            A = pow(a, 2)
+            B = pow(b, 2)
+            C = pow(c, 2)
             if A < B + C:
                 print('Acute-angled')
             elif A == B + C:
                 print('Rectangular')
             elif A > B + C:
                 print('Obtuse')
-            if a != b and b != c and c != a :
+            if a != b and b != c and c != a:
                 print('Іcalene')
             elif a == b != c or b == c != a or a == c != b:
                 print('Isosceles')
             elif a == b and b == c and c == a:
                 print('Equilateral')
 
-        else:
-            print('Such a triangle cannot exist.')
 
-
-
-
-
-Triangle1 = Triangle(3, 4, 5)
-#Triangle1 = Triangle(2, 3, 4)
-#Triangle1 = Triangle(66, 67, 68)
-#Triangle1 = Triangle(20, 20, 25)
-#Triangle1 = Triangle(5, 6, 20)
-#Triangle1 = Triangle('q', 'w', 'e')
-#Triangle1 = Triangle(0, 0, 0)
-#Triangle1 = Triangle(-2, -3, -5)
-#Triangle1 = Triangle(4294967295, 2147483647, 3647841458)
+# Triangle1 = Triangle(4, 3, 5)
+# Triangle1 = Triangle(1, 2, 3)
+# Triangle1 = Triangle(66, 67, 68)
+# Triangle1 = Triangle(-20, 20, 25)
+# Triangle1 = Triangle(5, 6, 20)
+# Triangle1 = Triangle('q', 'w', 'e')
+# Triangle1 = Triangle(0, 0, 0)
+# Triangle1 = Triangle('#', '^', '&')
+# Triangle1 = Triangle(-2, -3, -5)
+# Triangle1 = Triangle(4294967295, 2147483647, 3647841458)
 Triangle1.perimetr()
 Triangle1.square()
 Triangle1.type()
